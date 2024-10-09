e6aa7904bf867aad4583aed8b9c24cb959295aef
oliverleomendes <leo.jodo@gmail.com>
2024-10-09 19:12:27 -0300

    Inicialização das rotas de usuários.

--- model/modelUsers.php
+++ model/modelUsers.php
@@ -68,4 +68,193 @@ class modelUsers {
         }
     }
 
+    public function saveGroup($id_user, $group) {
+        try {
+            
+            $conn = connectionDB::connect();
+            $saveGroup = $conn->prepare("INSERT INTO tblusersroles VALUES (':id_user', ':group') ");
+            $saveGroup->bindParam(':id_user', $id_user);
+            $saveGroup->bindParam(':group', $group);
+            $saveGroup->execute();
+
+            return true;
+
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function auth($data) {
+        try {
+
+            $username = htmlspecialchars($data["username"], ENT_NOQUOTES);
+            
+            $conn = connectionDB::connect();
+            $auth = $conn->prepare("SELECT * FROM tblUsers WHERE username = ':username' ");
+            $auth->bindParam(':username', $username);
+            $auth->execute();
+            $result = $auth->fetch(PDO::FETCH_ASSOC);
+
+            if($result) {
+                $passwordDB = $result->pass_user;
+
+                $validatePassword = password_verify($data->password . $this->salt, $passwordDB);
+
+                if($validatePassword) {
+                    return $result;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function generateTwoFactor($data) {
+        try {
+
+            //Dados recebidos da requisição
+            $username = htmlspecialchars($data["username"], ENT_NOQUOTES);
+
+            //Expiração do token em 15 minutos
+            $expired_at = date('d/m/Y H:i:s', time() + (15 * 60));
+            
+            //Gerar token com a data e hora atual com nome do usuário
+            $token = md5(date('d/m/Y H:i:s') . $data->username );
+            $finalToken = substr($token, 6);
+
+            //Gravar os dados do token
+            $conn = connectionDB::connect();
+            $saveToken = $conn->prepare("INSERT INTO tblTokens VALUES (':token', ':username', ':expired') ");
+            $saveToken->bindParam(':token', $finalToken);
+            $saveToken->bindParam(':username', $username);
+            $saveToken->bindParam(':expired', $expired_at);
+            $saveToken->execute();
+
+            if($saveToken) {
+                //Texto do corpo do e-mail
+                $message = "Utilize o token: $finalToken";
+
+                $sendMail = mail($username, 'Token', $message);
+
+                if($sendMail) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function validateTwoFactor($data) {
+        try {
+        
+            $token = htmlspecialchars($data["token"], ENT_NOQUOTES);
+            $username = htmlspecialchars($data["username"], ENT_NOQUOTES);
+            
+            $conn = connectionDB::connect();
+            $validate = $conn->prepare("SELECT * FROM tblTokens WHERE username = ':username' AND token = ':token'");
+            $validate->bindParam(':username', $username);
+            $validate->bindParam(':token', $token);
+            $validate->execute();
+            $result = $validate->fetch(PDO::FETCH_ASSOC);
+
+            //Obter data e hora atual
+            $now = date('d/m/Y H:i:s');
+            //Converter data atual para validar a expiração
+            $date = strtotime($now); //121354545
+            //Converter data expiração para validar
+            $expired_at = strtotime($result["expired_at"]); //112545454           
+
+            //Deletar o token após uso
+            $deleteToken = $conn->prepare("DELETE FROM tblTokens WHERE username = ':username' AND token = ':token'");
+            $deleteToken->bindParam(":username", $username);
+            $deleteToken->bindParam(":token", $token);
+            $deleteToken->execute();
+
+            //Validar se a date e hora atual é superior a data expiração
+            if($date > $expired_at) {
+                return false;
+            } else {
+                return true;
+            }
+
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function listAll() {
+        try {
+            
+            $conn = connectionDB::connect();
+            $list = $conn->query("SELECT * FROM tblUsers");
+            $result = $list->fetchAll(PDO::FETCH_ASSOC);
+
+            return $result;
+
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function searchById($id) {
+        try {
+            
+            $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+
+            $conn = connectionDB::connect();
+            $search = $conn->prepare("SELECT * FROM tblUsers WHERE id_user = ':id_user'");
+            $search->bindParam(":id_user", $id);
+            $search->execute();
+            $result = $search->fecth(PDO::FETCH_ASSOC);
+
+            return $result;
+
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function delete($id) {
+        try {
+         
+            $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+
+            $conn = connectionDB::connect();
+            $delete = $conn->prepare("DELETE FROM tblUsers WHERE id_user = ':id_user'");
+            $delete->bindParam(":id_user", $id);
+            $delete->execute();
+
+            return true;
+            
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function update($id, $data) {
+        try {
+            
+            $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+            $firstname = htmlspecialchars($data["firstname"], ENT_NOQUOTES);
+            $lastname = htmlspecialchars($data["lastname"], ENT_NOQUOTES);
+            $mail = htmlspecialchars($data["mail"], ENT_NOQUOTES);
+            $password = htmlspecialchars($data["password"], ENT_NOQUOTES);
+            $status = filter_var($data["status"], FILTER_SANTIZE_NUMBER_INT);
+
+
+        } catch (PDOExceptio $e) {
+            return false;
+        }
+    }
+
 }
